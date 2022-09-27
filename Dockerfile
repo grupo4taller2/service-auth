d77630ea061961717c14540b841bc817e9bc8f65
mateoicalvo <macalvo@fi.uba.ar>
2022-09-27 05:48:35 -0300

    Fix linting and Dockerfile for local development

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,6 @@ ARG APP_ENV=production
 
 FROM python:3.10-slim-bullseye AS base
 
-RUN addgroup --system fiuber && adduser --system --group fiuber
 
 WORKDIR /opt/app/
 
@@ -11,15 +10,16 @@ ENV PYTHONUNBUFFERED 1
 
 # COPY requirements.txt alembic.ini ./
 COPY requirements.txt ./
-RUN chown -R fiuber:fiuber /opt/app /tmp && \
-    pip install -r requirements.txt
+RUN pip install -r requirements.txt
 
 FROM base as production-preinstall
 # RUN echo "copying necesary files for PROD"
+RUN addgroup --system fiuber && adduser --system --group fiuber
 COPY requirements-prod.txt ./
 COPY src ./src
+RUN chown -R fiuber:fiuber /opt/app /tmp && \
+    pip install -r requirements-prod.txt
 # COPY alembic ./alembic
-RUN  pip install -r requirements-prod.txt
 USER fiuber
 
 CMD bash -c 'sleep 5 && alembic upgrade head && python3 -m uvicorn src.main:app --host=0.0.0.0 --port=$PORT'
@@ -28,7 +28,6 @@ FROM base as development-preinstall
 # RUN echo "Installing necesary libs for DEV"
 COPY requirements-dev.txt ./
 RUN pip install -r requirements-dev.txt
-USER fiuber
 CMD bash
 
 FROM ${APP_ENV}-preinstall as final
--- src/main.py
+++ src/main.py
@@ -1,20 +1,20 @@
 from fastapi import FastAPI
-#from app.Controllers.inscription import InscriptionController
-#from app.Models.Requests.inscription import Inscription
-#from app.Models.Responses.inscriptions import Inscriptions
-#from app.Models.Responses.message import Message
+# from app.Controllers.inscription import InscriptionController
+# from app.Models.Requests.inscription import Inscription
+# from app.Models.Responses.inscriptions import Inscriptions
+# from app.Models.Responses.message import Message
 
 app = FastAPI()
-#inscriptions_repository = []
+# inscriptions_repository = []
 
 
-#@app.get("/inscriptions", response_model=Inscriptions)
-#def getInscriptions():
+# @app.get("/inscriptions", response_model=Inscriptions)
+# def getInscriptions():
 #    return InscriptionController().get(inscriptions_repository)
 
 
-#@app.post("/inscriptions", status_code=201, response_model=Message)
-#def postInscriptions(body: Inscription):
+# @app.post("/inscriptions", status_code=201, response_model=Message)
+# def postInscriptions(body: Inscription):
 #    return InscriptionController().post(inscriptions_repository, body)
 
 
@@ -23,8 +23,8 @@ def ping():
     return {"message": "UP"}
 
 
-#@app.post("/reset", response_model=Message)
-#def reset():
+# @app.post("/reset", response_model=Message)
+# def reset():
     # Rollback de la app
 #    inscriptions_repository.clear()
 #    return {"message": "ok"}
