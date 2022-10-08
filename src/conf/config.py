0720ac8bc1b82ad3d97debce62d95995dd9e82a4
Mateo <macalvo@fi.uba.ar>
2022-10-08 02:50:29 -0300

    Update settings

--- src/conf/config.py
+++ src/conf/config.py
@@ -4,7 +4,7 @@ import os
 
 
 class Settings(BaseSettings):
-    DATABASE_URI: str
+    DATABASE_URI: str = os.environ.get("DATABASE_URI")
     API_V1_STR: str = os.environ.get("API_VERSION_PREFIX")
     # TODO: Maybe move the following two to env vars?
     CRYPT_CONTEXT_SCHEME: List[str] = ['bcrypt']
